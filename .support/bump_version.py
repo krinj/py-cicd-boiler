47a0025ef0e56cbb23053eadc044cdb081c9428c
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-08-01 14:28:37 +0800

    Added Support Tools

--- .support/bump_version.py
+++ .support/bump_version.py
@@ -8,14 +8,14 @@ import os
 
 if __name__ == "__main__":
 
-    with open("support/version", "r") as f:
+    with open(".support/version", "r") as f:
         current_version = f.readline()
         versions = current_version.split(".")
         versions[-1] = str(int(versions[-1]) + 1)
         new_version = ".".join(versions)
 
-    with open("support/version", "w") as f:
+    with open(".support/version", "w") as f:
         f.write(new_version)
 
     print("Bumping Version Number: {} -> {}".format(current_version, new_version))
-    os.system("git add support/version")
+    os.system("git add .support/version")
