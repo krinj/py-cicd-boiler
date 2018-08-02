7ddb77a6c4eef6fb222b03daa9df11cdb7b3004f
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-08-02 09:52:11 +0800

    Fixed Another Version Bug

--- boiler/__init__.py
+++ boiler/__init__.py
@@ -7,6 +7,7 @@
 __author__ = "Jakrin Juangbhanich"
 __copyright__ = "Copyright 2018, GenVis Pty Ltd."
 __email__ = "krinj@genvis.co"
+__version__ = "0.0.0"
 
 # Make these accessible at the root namespace.
 from .boiler import Boiler
